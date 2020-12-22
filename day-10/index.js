d3f6890023786b9c435e84ea230f336e927c488e
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-12-22 15:07:54 -0500

    completed day 10, part 2

--- day-10/index.js
+++ day-10/index.js
@@ -111,9 +111,118 @@ fs.readFile('./input.txt', 'utf8', (err, data) => {
 
   const product = oneJoltCounter * threeJoltCounter
 
-  console.log(product)
+  // console.log(product)
 })
 
 /*
+--- Part Two ---
+To completely determine whether you have enough adapters, you'll need to figure out how many different ways they can be arranged. Every arrangement needs to connect the charging outlet to your device. The previous rules about when adapters can successfully connect still apply.
 
+The first example above (the one that starts with 16, 10, 15) supports the following arrangements:
+
+(0), 1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, (22)
+(0), 1, 4, 5, 6, 7, 10, 12, 15, 16, 19, (22)
+(0), 1, 4, 5, 7, 10, 11, 12, 15, 16, 19, (22)
+(0), 1, 4, 5, 7, 10, 12, 15, 16, 19, (22)
+(0), 1, 4, 6, 7, 10, 11, 12, 15, 16, 19, (22)
+(0), 1, 4, 6, 7, 10, 12, 15, 16, 19, (22)
+(0), 1, 4, 7, 10, 11, 12, 15, 16, 19, (22)
+(0), 1, 4, 7, 10, 12, 15, 16, 19, (22)
+(The charging outlet and your device's built-in adapter are shown in parentheses.) Given the adapters from the first example, the total number of arrangements that connect the charging outlet to your device is 8.
+
+The second example above (the one that starts with 28, 33, 18) has many arrangements. Here are a few:
+
+(0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+32, 33, 34, 35, 38, 39, 42, 45, 46, 47, 48, 49, (52)
+
+(0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+32, 33, 34, 35, 38, 39, 42, 45, 46, 47, 49, (52)
+
+(0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+32, 33, 34, 35, 38, 39, 42, 45, 46, 48, 49, (52)
+
+(0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+32, 33, 34, 35, 38, 39, 42, 45, 46, 49, (52)
+
+(0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+32, 33, 34, 35, 38, 39, 42, 45, 47, 48, 49, (52)
+
+(0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+46, 48, 49, (52)
+
+(0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+46, 49, (52)
+
+(0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+47, 48, 49, (52)
+
+(0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+47, 49, (52)
+
+(0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+48, 49, (52)
+In total, this set of adapters can connect the charging outlet to your device in 19208 distinct arrangements.
+
+You glance back down at your bag and try to remember why you brought so many adapters; there must be more than a trillion valid ways to arrange them! Surely, there must be an efficient way to count the arrangements.
+
+What is the total number of distinct ways you can arrange the adapters to connect the charging outlet to your device?
 */
+
+fs.readFile('./input.txt', 'utf8', (err, data) => {
+  if (err) console.log(err)
+
+  const parsedData = data.split('\n')
+
+  if (parsedData[parsedData.length-1].length === 0) parsedData.pop()
+
+  for (let i = 0; i < parsedData.length; i++) parsedData[i] = Number(parsedData[i])
+
+  parsedData.sort( (a,b) => a - b)
+
+  console.log(parsedData)
+
+  const dp = []
+
+  for (let i = 0; i < parsedData.length; i++) {
+    let sum
+
+    // This if-else statement takes into account connecting to the outlet itself.
+    // Any adapter that connects directly to the outlet must count towards the arrangement itself, hence sum = 1
+    if (parsedData[i] <= 3) {
+      sum = 1
+    } else {
+      sum = 0
+    }
+
+    if (parsedData[i] - parsedData[i-1] <= 3) sum += dp[i-1]
+    if (parsedData[i] - parsedData[i-2] <= 3) sum += dp[i-2]
+    if (parsedData[i] - parsedData[i-3] <= 3) sum += dp[i-3]
+    dp[i] = sum
+  }
+  /*
+  let count = 0
+
+  function jumpVoltage(index) {
+    // console.log(index, parsedData[index])
+
+    let currentJoltage = parsedData[index]
+
+    // can use the index as base case because difference between the device voltage and last adapter will always be 3.
+    // hence, you must arrange with the last adapter no matter what.
+    if (index === parsedData.length-1) {
+      count++
+      return
+    }
+
+    if (parsedData[index+1] - currentJoltage <= 3) jumpVoltage(index+1)
+    if (parsedData[index+2] - currentJoltage <= 3) jumpVoltage(index+2)
+    if (parsedData[index+3] - currentJoltage <= 3) jumpVoltage(index+3)
+  }
+
+  if (parsedData[0] <= 3) jumpVoltage(0)
+  if (parsedData[1] <= 3) jumpVoltage(1)
+  if (parsedData[2] <= 3) jumpVoltage(2)
+  */
+
+  console.log(dp[dp.length-1])
+})
