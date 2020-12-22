a54ae90b78087af1bb4c0faded8e56b4b5b1b07f
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-12-21 23:46:04 -0500

    completed day 9, part two

--- day-9/index.js
+++ day-9/index.js
@@ -85,5 +85,98 @@ fs.readFile('./input.txt', 'utf8', (err, data) => {
     right++
   }
 
-  console.log(result)
+  // console.log(result)
+})
+
+/*
+--- Part Two ---
+The final step in breaking the XMAS encryption relies on the invalid number you just found: you must find a contiguous set of at least two numbers in your list which sum to the invalid number from step 1.
+
+Again consider the above example:
+
+35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576
+In this list, adding up all of the numbers from 15 through 40 produces the invalid number from step 1, 127. (Of course, the contiguous set of numbers in your actual list might be much longer.)
+
+To find the encryption weakness, add together the smallest and largest number in this contiguous range; in this example, these are 15 and 47, producing 62.
+
+What is the encryption weakness in your XMAS-encrypted list of numbers?
+*/
+
+fs.readFile('./input.txt', 'utf8', (err, data) => {
+  if (err) console.log(err)
+
+  const parsedData = data.split('\n')
+
+  const PREAMBLE = 25
+
+  let left = 0
+  let right = PREAMBLE - 1
+  let result
+
+  function twoSum(left, right, target) {
+    const indices = {}
+
+    for (let i = left; i <= right; i++) {
+      indices[parsedData[i]] = true
+    }
+
+    for (let i = left; i <= right; i++) {
+      let difference = Number(target) - parsedData[i]
+      if (indices[difference]) return true
+    }
+
+    return false
+  }
+
+  for (let i = PREAMBLE; i < parsedData.length; i++) {
+    if (!twoSum(left, right, parsedData[i])) {
+      result = parsedData[i]
+      break
+    }
+    left++
+    right++
+  }
+
+  result = Number(result)
+
+  let leftPointer = 0
+  let rightPointer = 1
+
+  let sum = Number(parsedData[leftPointer]) + Number(parsedData[rightPointer])
+
+  while (rightPointer < parsedData.length) {
+    if (sum < result) {
+      rightPointer++
+      sum += Number(parsedData[rightPointer])
+    } else if (sum > result){
+      sum -= Number(parsedData[leftPointer])
+      leftPointer++
+    } else {
+      break
+    }
+  }
+
+  let smaller = Math.min(...parsedData.slice(leftPointer, rightPointer+1))
+  let larger = Math.max(...parsedData.slice(leftPointer,rightPointer+1))
+  let invalidSum = smaller + larger
+  console.log(invalidSum)
 })
